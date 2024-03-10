2c834f3d77967b47556999a0537f53cc8d716fa6
Mai Sato <mu.mat.sig@gmail.com>
2024-03-11 07:59:07 +0900

    modified

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', '127.0.0.1:8080', 'https://mymys-frontend-g3ez.vercel.app/'
+    origins 'localhost:8080', '127.0.0.1:8080', 'https://mymys-frontend-g3ez.vercel.app'
 
     resource "*",
       headers: :any,
