eb5d43b074420ca028367a4466f5a7472330940e
Mai Sato <mu.mat.sig@gmail.com>
2024-03-17 22:44:24 +0900

    バグ修正とcors確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  # curl使用時用に残しておく
+  # def take_name(count)
+  #   url = "https://green.adam.ne.jp/roomazi/cgi-bin/randomname.cgi?n=#{count}&callback=players"
+  #   uri = URI.parse(url)
+  #   request = Net::HTTP::Get.new(uri)
+  #   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+  #       http.request(request)
+  #   end
+  #   body = response.body
+  #   callback_name = 'players'
+  #   parsed_data = JSON.parse(body.sub(/^#{callback_name}\((.*)\)$/, '\1'))
+  #   parsed_data["name"].each do |name|
+  #
+  #   end
+  # end
 end
