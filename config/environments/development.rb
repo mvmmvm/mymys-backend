962dc6867138b206b0c07000314629dd25f78df1
Mai Sato <mu.mat.sig@gmail.com>
2024-04-18 14:25:40 +0900

    cors,env修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,11 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.hosts << 'localhost'
+  config.hosts << '127.0.0.1'
+  config.hosts << ENV['BACKEND_HOST']
+  config.hosts << '192.168.10.106'
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,10 +9,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', '127.0.0.1:8080', 'https://mymys-frontend-g3ez.vercel.app'
+    origins 'localhost:8080', '127.0.0.1:8080', 'https://mymys-frontend-g3ez.vercel.app', 'http://192.168.10.106:8080', 'https://enhanced-especially-piranha.ngrok-free.app', 'https://access.line.me/oauth2/v2.1/login'
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
